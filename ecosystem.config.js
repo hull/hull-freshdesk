3ea5e0827c8d4f391c972e164955e601a8e7f57e
Sven Maschek <smaschek@outlook.com>
2020-06-23 09:35:04 +0200

    Fix label in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,16 @@
 module.exports = {
-  apps: [
-    {
-      name: "Hull Salesforce Dependant Custom Objects",
-      script: "dist/index.js",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: "450M",
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      },
+  apps: [{
+    name: "Hull Freshdesk",
+    script: "dist/index.js",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "450M",
+    env: {
+      NODE_ENV: "development",
     },
-  ],
+    env_production: {
+      NODE_ENV: "production",
+    },
+  }, ],
 };
